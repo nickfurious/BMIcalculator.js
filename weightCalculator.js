ec8feb7da3a3e1a6e8358adec794bb6fab398378
Nick Hale <53896693+nickfurious@users.noreply.github.com>
2020-01-26 11:28:16 -0800

    Create weightCalculator.js

---
+++ weightCalculator.js
@@ -0,0 +1,30 @@
+var john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+}
+
+var mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+}
+
+john.calcBMI();
+mark.calcBMI();
+
+if (john.calcBMI() > mark.calcBMI()) {
+  console.log(john.fullName + ' has a higher BMI of ' + john.bmi);
+} else if (mark.bmi > john.bmi) {
+  console.log(mark.fullName + ' has a higher BMI of ' + mark.bmi);
+} else {
+  console.log('they have the same BMI');
+}
